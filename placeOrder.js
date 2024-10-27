b9afbed6f96ea2eb303ab3398406593a6a674b2e
Jasper-ops698 <bkitib2@gmail.com>
2024-10-27 18:29:44 +0300

    Update placeOrder.js

--- placeOrder.js
+++ placeOrder.js
@@ -1,15 +1,3 @@
-import OrderForm from'./bus.js';
-
-fucntion App() {
-  return (
-    <div>
-        <h1>Order Form</h1>
-        <OrderForm />
-    </div>
-  );
-}
-
-export default App;
 
 const { MongoClient } = require('mongodb');
 const nodemailer = require('nodemailer');
